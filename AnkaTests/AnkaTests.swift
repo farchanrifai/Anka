116c356cd68f917476fe23ad5c6e5ee6ba8ea7d8
Farchan <farchanrifaii@gmail.com>
2026-06-06 20:43:33 +0700

    Initial Commit

---
+++ AnkaTests/AnkaTests.swift
@@ -0,0 +1,19 @@
+//
+//  AnkaTests.swift
+//  AnkaTests
+//
+//  Created by Farchan on 6/6/2026.
+//
+
+import Testing
+@testable import Anka
+
+struct AnkaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
